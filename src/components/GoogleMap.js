857420c190c9c01e1b0de2631655ed438e34c052
Sam Stanton-Reid <samstantonreid@gmail.com>
2017-10-25 23:28:59 -0700

    Import components from another project

---
+++ src/components/GoogleMap.js
@@ -0,0 +1,149 @@
+import React from 'react';
+
+class GoogleMap extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.createMap = this.createMap.bind(this);
+    this.loadData = this.loadData.bind(this);
+    this.checkLoaded = this.checkLoaded.bind(this);
+
+    this.state = {
+      zoom: 3
+    };
+  }
+
+  createMap() {
+    const map = google.maps;
+
+    var options = {
+      center: new map.LatLng(
+        this.props.lat,
+        this.props.lng
+      ),
+      zoom: this.state.zoom
+    };
+
+    this.map = new map.Map(this.mapDiv, options);
+  }
+
+  isLoaded() {
+    return typeof window !== 'undefined' && window.google !== 'undefined' && typeof window.google.maps !== 'undefined';
+  }
+
+  loadData(data) {
+    var map = this.map;
+
+    map.data.addGeoJson(data);
+    map.data.setStyle(this.props.style);
+
+    if (this.props.mouseOver) {
+      map.data.addListener('mouseover', this.props.mouseOver.bind(this));
+    }
+
+    if (this.props.onClick) {
+      map.data.addListener('click', this.props.onClick.bind(this));
+    }
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  componentDidMount() {
+    if (this.isLoaded()) {
+      this.createMap();
+
+      if (this.props.data) {
+        this.loadData(this.props.data);
+      }
+    } else {
+      this.interval = setTimeout(this.checkLoaded, 1000);
+    }
+  }
+
+  checkLoaded() {
+    if (this.isLoaded()) {
+      clearInterval(this.interval);
+      this.createMap();
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (this.isLoaded()) {
+      const {center, data, bounds, marker} = newProps;
+
+      if (data && data.features.length !== this.props.data.features.length) {
+        this.loadData(data);
+      }
+
+      if (JSON.stringify(center) !== JSON.stringify(this.props.center)) {
+        var latLng = new google.maps.LatLng(center.lat, center.lng);
+        this.map.setCenter(latLng);
+        this.map.setZoom(center.zoom);
+      }
+
+      if (JSON.stringify(bounds) !== JSON.stringify(this.props.bounds)) {
+        var googleBounds = new google.maps.LatLngBounds(
+          new google.maps.LatLng(bounds[0][1], bounds[0][0]),
+          new google.maps.LatLng(bounds[1][1], bounds[1][0])
+        );
+
+        this.map.fitBounds(googleBounds);
+      }
+
+      if (JSON.stringify(marker) !== JSON.stringify(this.props.marker)) {
+        let latLng = new google.maps.LatLng(marker.lat, marker.lng);
+        var googleMarker = new google.maps.Marker({
+          position: latLng,
+          map: this.map
+        });
+
+        var infowindow = new google.maps.InfoWindow({
+          content: marker.marker
+        });
+
+        googleMarker.addListener('click', function () {
+          infowindow.open(this.map, googleMarker);
+        });
+
+        this.map.setCenter(latLng);
+        this.map.setZoom(marker.zoom);
+      }
+    } else {
+      clearTimeout(this.interval);
+      this.interval = setTimeout(this.checkLoaded, 1000);
+    }
+  }
+
+  render() {
+    const { height, width } = this.props;
+    var style = {};
+
+    if (height && width) {
+      style= {height: height, width: width};
+    }
+
+    return (
+      <div
+        style={style}
+        className={"google-map-container " + this.props.className}>
+        <div className="google-map" ref={map => this.mapDiv = map}>
+        </div>
+      </div>
+    );
+  }
+}
+
+GoogleMap.defaultProps = {
+  lng: -98,
+  lat: 35,
+  center: [],
+  bounds: [[0, 0], [5, 5]]
+};
+
+export default GoogleMap;
---
+++ src/components/NationalParks.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import GoogleMap from './GoogleMap';
+import * as d3 from 'd3';
+import {fetchFeaturesIfNeeded} from '../actions/features';
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+import {bindActionCreators} from 'redux'
+
+class NationalParks extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.mouseOver = this.mouseOver.bind(this);
+    this.renderTableRow = this.renderTableRow.bind(this);
+
+    this.types = [];
+
+    this.state = {
+      bounds: [[], []],
+      name: ''
+    }
+  }
+
+  componentWillMount() {
+    this.props.fetchFeaturesIfNeeded('park');
+  }
+
+  componentWillReceiveProps(newProps) {
+    const features = newProps.featureCollection.features;
+    if (features && features.length > 0) {
+      this.types = this.removeDuplicates(features);
+    }
+  }
+
+  removeDuplicates(arr) {
+    let visited = {};
+
+    for (let i = 0; i < arr.length; i++) {
+      let type = arr[i].properties.UNIT_TYPE === '' ? 'null' : arr[i].properties.UNIT_TYPE;
+
+      if (!visited.hasOwnProperty(type)) {
+        visited[type] = 1;
+      } else {
+        visited[type]++;
+      }
+    }
+
+    return visited;
+  }
+
+  setInfoBox(name, type) {
+    document.getElementById('box').textContent = name + (type === '' ? '' : ', ' + type);
+  }
+
+  mouseOver(e) {
+    var type = e.feature.getProperty('UNIT_TYPE');
+    this.setInfoBox(e.feature.getProperty('UNIT_NAME'), type);
+  }
+
+  handleClick(e) {
+    var bounds = d3.geoBounds(e);
+    var type = e.properties.UNIT_TYPE;
+
+    this.setInfoBox(e.properties.UNIT_NAME, type);
+    this.setState({bounds: bounds});
+  }
+
+  getRowText(feature) {
+    return feature.properties.UNIT_NAME + ' (' + feature.properties.UNIT_TYPE + ') ';
+  }
+
+  renderTableRow(feature) {
+    return (
+      <button
+        onClick={this.handleClick.bind(this, feature)}
+        type="button"
+        className="list-group-item">
+        {this.getRowText(feature)}
+        <i className="fa fa-map-marker"/>
+      </button>
+    )
+  }
+
+  render() {
+    if (this.props.loading) {
+      return (
+        <div className="jumbotron text-center">
+          <i className="fa fa-cog fa-spin fa-4x"/>
+        </div>
+      )
+    }
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20).domain(Object.keys(this.types));
+
+    return (
+      <div className="national-parks">
+        <div className="park-grid">
+          <div className="text-center justify">
+            <h2 style={{margin: 0}}>US National Parks</h2>
+          </div>
+          <div className="park-map">
+            <h2 className="text-center justify" id="box">Explore the map or table beneath</h2>
+            <GoogleMap
+              height="100%"
+              width="100%"
+              bounds={this.state.bounds}
+              style={(feature, i) => {
+              return {fillColor: color(feature.f.UNIT_TYPE), strokeOpacity: '0.5'}
+            }}
+              data={this.props.featureCollection}
+              mouseOver={this.mouseOver}
+            />
+          </div>
+          <div className="park-table">
+            <div
+              className="list-group">
+              {this.props.featureCollection.features.map(feature => this.renderTableRow(feature, color))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+NationalParks.defaultProps = {
+  featureCollection: {type: 'FeatureCollection', features: []}
+};
+
+
+const mapStateToProps = state => {
+  const {featuresByCollection} = state;
+  const {
+    loading,
+    featureCollection
+  } = featuresByCollection['park'] || {
+    loading: true,
+    featureCollection: {"type": "FeatureCollection", "features": []}
+  };
+
+  return {loading, featureCollection};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {fetchFeaturesIfNeeded: bindActionCreators(fetchFeaturesIfNeeded, dispatch)}
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NationalParks));
