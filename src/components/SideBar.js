8edc58c733ae1b23f2eb7bbc3b82a7f3daa6b627
Sam Stanton-Reid <stantonr@bu.edu>
2018-01-19 20:26:09 -0800

    Create SideBar component

---
+++ src/components/SideBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+class SideBar extends React.Component {
+  render() {
+    return (
+      <div className="col-12 col-sm-3 col-lg-2 side-bar">
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <NavLink to='/account' exact={true} activeStyle={{color: 'rgba(255, 0, 0, 1)'}}>Account Home</NavLink>
+          </li>
+          {this.props.routes.map((route, i) => (
+            <li className="nav-item" key={i}>
+              <NavLink
+                to={route.path}
+                activeStyle={{color: 'rgba(255, 0, 0, 1)'}}>
+                {route.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
