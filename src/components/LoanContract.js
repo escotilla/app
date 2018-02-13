f6abf159e0eb42cad9e497ffd8d9a8743cfe4af2
Sam Stanton-Reid <stantonr@bu.edu>
2018-02-12 22:09:35 -0800

    Implement loan contract form

--- src/components/LoanContract.js
+++ src/components/LoanContract.js
@@ -2,28 +2,35 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {logout} from '../actions/logout';
 import {bindActionCreators} from 'redux'
+import LoanContractForm from './Forms/LoanContractForm';
+
 class LoanContract extends React.Component {
   render() {
-    const { user } = this.props;
+    const {user} = this.props;
 
     return (
-      <div>
-        <h1>Loan Contract page</h1>
-        <p>You are logged in as {user.email}</p>
-        <h1>Please accept our contract</h1>
-
-        <h3 onClick={() => this.props.logout()}> LOGOUT </h3>
+      <div>{user.applications.map(app => (
+        <div>
+          <h1>Loan Contract page</h1>
+          <p>You are logged in as {user.email}</p>
+          <h1>Please accept our contract</h1>
+          <LoanContractForm
+            applicationId={app.id}
+            answers={app.answers}/>
+          <h3 onClick={() => this.props.logout()}> LOGOUT </h3>
+        </div>
+      ))}
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => {
   const {
-    user,
+    user
   } = state;
 
-  return { user };
+  return {user};
 };
 
 const mapStateToDispatch = dispatch => {
