66e7d32115043726f981a44f0e5e1ee59e5156b5
Sam Stanton-Reid <stantonr@bu.edu>
2018-02-15 22:39:14 -0800

    Update language for new input design

--- src/configs/language.js
+++ src/configs/language.js
@@ -1,11 +1,13 @@
-export default {
+import Q from './questions';
+let lang = {
   english: {
     english: 'English',
     spanish: 'Español (Spanish)',
     button: {
       login: 'Login',
       register: 'Register',
-      get_started: 'Let\'s get started!'
+      get_started: 'Let\'s get started!',
+      passwordMatch: 'Passwords must match'
     },
     home: {
       header: {
@@ -43,7 +45,8 @@ export default {
     errors: {
       amountMax: 'Amount no puede exceed $200',
       required: '*requiredo',
-      info_accurate: 'Please make sure your info is accurate'
+      info_accurate: 'Please make sure your info is accurate',
+      passwordMatch: 'Passwords must match'
     },
     checklist: {
       review_profile: 'Review Profile',
@@ -51,4 +54,134 @@ export default {
       sign_agreement: 'Loan Contract',
     }
   }
-}
+};
+
+lang.spanish[Q.BUSINESS_DESCRIPTION] = {
+  label: "Describe your business por favor",
+  placeholder: "Landscaping",
+  helper: ""
+};
+
+lang.english[Q.BUSINESS_DESCRIPTION] = {
+  label: "Please describe your business",
+  placeholder: "Landscaping",
+  helper: ""
+};
+
+lang.spanish[Q.DOB] = {
+  label: "Date of Birth",
+  placeholder: "",
+  helper: ""
+};
+
+lang.english[Q.DOB] = {
+  label: "Date of Birth",
+  placeholder: "",
+  helper: ""
+};
+
+lang.spanish[Q.BUSINESS_PRODUCT] = {
+  label: "What product or service do you sell?",
+  placeholder: "Citrus fruits",
+  helper: ""
+};
+
+lang.english[Q.BUSINESS_PRODUCT] = {
+  label: "What product or service do you sell?",
+  placeholder: "",
+  helper: ""
+};
+
+lang.spanish[Q.BUSINESS_NAME] = {
+  label: "Business Name",
+  placeholder: "Juan's Fruits",
+  helper: ""
+};
+
+lang.english[Q.BUSINESS_NAME] = {
+  label: "Business Name",
+  placeholder: "Juan's Tienda",
+  helper: ""
+};
+
+lang.spanish[Q.LOAN_AMOUNT] = {
+  label: "Loan Amount",
+  placeholder: "$50",
+  helper: "You may request up to $200"
+};
+
+lang.english[Q.LOAN_AMOUNT] = {
+  label: "Loan Amount",
+  placeholder: "$50",
+  helper: "You may request up to $200"
+};
+
+lang.spanish[Q.FULL_NAME] = {
+  label: "Full Name",
+  placeholder: "Gabriel Enrique Gómez Torres",
+  helper: "Please enter your full legal name"
+};
+
+lang.english[Q.FULL_NAME] = {
+  label: "Full Name",
+  placeholder: "Gabriel Enrique Gómez Torres",
+  helper: "Please enter your full legal name"
+};
+
+lang.spanish[Q.EMAIL] = {
+  label: "Email",
+  placeholder: "gabrieltorres292@gmail.com",
+  helper: "This will also be your username"
+};
+
+lang.english[Q.EMAIL] = {
+  label: "Email",
+  placeholder: "gabrieltorres292@gmail.com",
+  helper: "This will also be your username"
+};
+
+lang.spanish[Q.INFO_ACCURATE] = {
+  label: "I have reviewed all my information for accuracy",
+  helper: ""
+};
+
+lang.english[Q.INFO_ACCURATE] = {
+  label: "I have reviewed all my information for accuracy",
+  helper: ""
+};
+
+lang.spanish[Q.AGREE_LOAN_CONTRACT] = {
+  label: "I agree to the terms and conditions",
+  helper: ""
+};
+
+lang.english[Q.AGREE_LOAN_CONTRACT] = {
+  label: "I agree to the terms and conditions",
+  helper: ""
+};
+
+lang.spanish[Q.PASSWORD] = {
+  label: "Password",
+  placeholder: "Password",
+  helper: "Must be at least 8 characters"
+};
+
+lang.english[Q.PASSWORD] = {
+  label: "Password",
+  placeholder: "Password",
+  helper: "Must be at least 8 characters"
+};
+
+lang.spanish['password'] = {
+  label: "Password",
+  placeholder: "Password",
+  helper: "Please reenter your password"
+};
+
+lang.english['password'] = {
+  label: "Password",
+  placeholder: "Password",
+  helper: "Please reenter your password"
+};
+
+export default lang;
