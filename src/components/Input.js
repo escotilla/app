bf7eeac3509323a574d824f71e22adf6ac448bf4
Sam Stanton-Reid <stantonr@bu.edu>
2018-02-15 22:36:23 -0800

    Make Input component more versatile

--- src/components/Input.js
+++ src/components/Input.js
@@ -6,46 +6,67 @@ import {bindActionCreators} from 'redux';
 import Language from '../utilities/language';
 
 class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updatePayload = this.updatePayload.bind(this);
+  }
+
+  updatePayload(e) {
+    const {
+      page,
+      question,
+    } = this.props;
+
+    const {
+      inputId,
+      parser
+    } = question;
+
+    this.props.updatePayload(inputId, page, (parser ? parser(e.target.value) : (e.target.type === 'checkbox' ? e.target.checked : e.target.value)));
+  }
   render() {
     const {
       validation,
       loading,
       value,
-      questions,
       language,
       placeholder,
-      page,
-      question
+      question,
+      helper,
+      questions
     } = this.props;
+
+    const formLabel = this.props.label;
 
     const {
       inputId,
       formatter,
-      parser,
       type,
       disabled,
     } = question;
     const hasError = validation && validation.hasOwnProperty(inputId);
 
     const feedback = hasError ? (
-      <div className="invalid-feedback">
+      <div className="helper invalid-feedback">
         {Language.get(language, validation[inputId][0])}
       </div>
-    ) : null;
+    ) : <div className="helper">
+      {helper !== undefined ? helper : Language.get(language, inputId + '.helper')}
+    </div>;
 
     return (
       <div className={"form-group " + (hasError ? 'has-error' : '')}>
-        <label>{questions.hasOwnProperty(inputId) ? questions[inputId][language] : ''}</label>
+        <label className="form-label">{Language.get(language, inputId + '.label') || formLabel}</label>
         <input
           required
           disabled={loading || disabled}
-          onChange={(e) => this.props.updatePayload(inputId, page, (parser ? parser(e.target.value) : (e.target.type === 'checkbox' ? e.target.checked : e.target.value)))}
+          onChange={this.updatePayload}
           value={formatter ? formatter(value) : value}
           checked={type === 'checkbox' ? value : null}
-          className="form-control form-transparent"
+          className="form-input form-transparent"
           id={inputId}
-          type={type || 'text'}
-          placeholder={questions[inputId][language] || placeholder}/>
+          type={questions.hasOwnProperty(inputId) ? questions[inputId]['input_type'] : question.type || 'text'}
+          placeholder={Language.get(language, inputId + '.placeholder') || placeholder}/>
         {feedback}
       </div>
     );
