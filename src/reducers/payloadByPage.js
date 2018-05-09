ed7620da12eefa882f84debd73e88e7911b6f998
Sam Stanton-Reid <stantonr@bu.edu>
2018-05-09 09:53:51 -0400

    Update payloadByPage store to listen for requests

--- src/reducers/payloadByPage.js
+++ src/reducers/payloadByPage.js
@@ -1,10 +1,10 @@
 import {
   UPDATE_PAYLOAD,
   REGISTER_SUCCESS,
-  LOGIN_SUCCESS,
+  REQUEST_SUCCESS,
   REGISTER_FAILURE,
-  LOGIN_FAILURE,
-  LOGIN_INIT,
+  REQUEST_FAILURE,
+  REQUEST_INIT,
   REGISTER_INIT,
   CLEAR_PAYLOAD,
   LOGOUT,
@@ -19,41 +19,41 @@ const payload = (state = {
   message: null,
   success: null
 }, action) => {
-    switch (action.type) {
-      case UPDATE_PAYLOAD:
-        return Object.assign({}, state, {
-          payload: updatePayload(state.payload, action)
-        });
-      case LOGIN_INIT:
-      case REGISTER_INIT:
-        return Object.assign({}, state, {
-          loading: true
-        });
-      case REGISTER_SUCCESS:
-      case LOGIN_SUCCESS:
-        return Object.assign({}, state, {
-          payload: INITIAL_STATE,
-          loading: false,
-          success: true,
-          error: null
-        });
-      case REGISTER_FAILURE:
-      case LOGIN_FAILURE:
-        return Object.assign({}, state, {
-          loading: false,
-          error: {
-            message: action.error.message || undefined,
-            code: action.error.code || 500
-          }
-        });
-      case CLEAR_PAYLOAD:
-        return Object.assign({}, state, {
-          payload: {}
-        });
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case UPDATE_PAYLOAD:
+      return Object.assign({}, state, {
+        payload: updatePayload(state.payload, action)
+      });
+    case REQUEST_INIT:
+    case REGISTER_INIT:
+      return Object.assign({}, state, {
+        loading: true
+      });
+    case REGISTER_SUCCESS:
+    case REQUEST_SUCCESS:
+      return Object.assign({}, state, {
+        payload: INITIAL_STATE,
+        loading: false,
+        success: true,
+        error: null
+      });
+    case REGISTER_FAILURE:
+    case REQUEST_FAILURE:
+      return Object.assign({}, state, {
+        loading: false,
+        error: {
+          message: action.error.message || undefined,
+          code: action.error.code || 500
+        }
+      });
+    case CLEAR_PAYLOAD:
+      return Object.assign({}, state, {
+        payload: {}
+      });
+    default:
+      return state;
+  }
+};
 
 const updatePayload = (state, action) => {
   switch (action.type) {
@@ -72,9 +72,9 @@ const payloadByPage = (state = {}, action) => {
     case REGISTER_FAILURE:
     case REGISTER_SUCCESS:
     case REGISTER_INIT:
-    case LOGIN_SUCCESS:
-    case LOGIN_FAILURE:
-    case LOGIN_INIT:
+    case REQUEST_SUCCESS:
+    case REQUEST_FAILURE:
+    case REQUEST_INIT:
     case CLEAR_PAYLOAD:
       return Object.assign({}, state, {
         [action.page]: payload(state[action.page], action)
